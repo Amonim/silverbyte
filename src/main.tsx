39a15a5d5e678eeb479172280e0d8c95fa18c693
Amonim <aryn.abiev@mail.ru>
2026-04-14 13:34:47 +0500

    before: 15

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import './styles/variables.css'
+import './styles/reset.css'
+import './styles/global.css'
+import './styles/layout.css'
+import './styles/home.css'
+import './styles/catalog.css'
+import './styles/product.css'
+import './styles/cart.css'
+import './styles/auth.css'
+import './styles/checkout.css'
+import './styles/profile.css'
+import './styles/chatbot.css'
+import './styles/theme.css'
+import './styles/responsive.css'
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
